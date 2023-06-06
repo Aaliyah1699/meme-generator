e512936683d0d7850ad5b893e6084f27eab71e88
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-06-06 16:26:54 -0500

    container div

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-  
-  )
+  return <div className="content-container"></div>;
 }
 
-export default App
+export default App;
