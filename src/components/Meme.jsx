12ee498ad5f9162fe70a11b3c7bade12965a1dcb
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-06-06 22:57:51 -0500

    placed inputs into container

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,18 +2,20 @@ const Meme = () => {
   return (
     <div className="meme-container">
       <form className="meme-form">
-        <input
-          className="top-input"
-          placeholder="#"
-          type="text"
-          name="top-input"
-        />
-        <input
-          className="bottom-input"
-          placeholder="#"
-          type="text"
-          name="bottom-input"
-        />
+        <div className="meme-input">
+          <input
+            className="top-input"
+            placeholder="#"
+            type="text"
+            name="top-input"
+          />
+          <input
+            className="bottom-input"
+            placeholder="#"
+            type="text"
+            name="bottom-input"
+          />
+        </div>
         <button className="meme-btn" type="submit">
           Get a new meme image 🖼️
         </button>
